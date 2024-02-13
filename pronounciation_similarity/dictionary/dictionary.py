0936d34bf6d67e29b4ec9d1b5996488c1072c90a
Acan <xie.zeyu20@gmail.com>
2024-02-13 12:36:48 +0800

    .

--- pronounciation_similarity/dictionary/dictionary.py
+++ pronounciation_similarity/dictionary/dictionary.py
@@ -25,11 +25,21 @@ def sort_f(chr):
 chr_list.sort(key = sort_f)
 
 # Output
+
+def cut_tone(_str):
+    if _str[-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        return _str[:-1]
+    return _str
+
 with open(os.path.join(os.path.dirname(__file__), "dictionary.json"), "w") as file:
     json.dump(tmp_2, file, indent = 4, ensure_ascii = False)
     file.close()
 with open(os.path.join(os.path.dirname(__file__), "dictionary_character_only.txt"), "w") as file:
     l = len(chr_list)
     for i in range(0, l):
+        if (l == 0) or (cut_tone(tmp_2[chr_list[i-1]][0]) != cut_tone(tmp_2[chr_list[i]][0])):
+            file.write("\n")
+            file.write("== "+cut_tone(tmp_2[chr_list[i]][0])+" ==")
+            file.write("\n")
         file.write(chr_list[i]+"\n")
     file.close()
